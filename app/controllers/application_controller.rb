86d21e344a581b00ed07c13237f40a8ee6fe613f
Luis Roberto <luisrobertopenav@gmail.com>
2022-09-09 19:26:05 -0700

    Restrict access and modify details

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
+  before_action :require_user
 
   def current_user
     @current_user ||= Student.find(session[:student_id]) if session[:student_id]
@@ -7,5 +8,12 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:notice] = 'You should be logged in to perform that action'
+      redirect_to login_path
+    end
   end
 end
